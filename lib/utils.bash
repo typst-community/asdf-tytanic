034bb89941742bc4f2158a0d1f633d5dfa7d46d8
Pascal Quach <pascal.quach@centralesupelec.fr>
2026-01-02 18:18:53 +0000

    asdf-tytanic

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,74 +1,112 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
+
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for tytanic.
 GH_REPO="https://github.com/typst-community/tytanic"
 TOOL_NAME="tytanic"
 TOOL_TEST="tt --version"
 
 fail() {
-	echo -e "asdf-$TOOL_NAME: $*"
-	exit 1
+    echo -e "asdf-$TOOL_NAME: $*"
+    exit 1
 }
 
 curl_opts=(-fsSL)
 
 # NOTE: You might want to remove this if tytanic is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+    curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
 sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+    sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+        LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    git ls-remote --tags --refs "$GH_REPO" |
+        grep -o 'refs/tags/.*' | cut -d/ -f3- |
+        sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if tytanic has other means of determining installable versions.
-	list_github_tags
+    list_github_tags
 }
 
 download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
+    local version filename url
+    version="$1"
+    filename="$2"
 
-	# TODO: Adapt the release URL convention for tytanic
-	url="$GH_REPO/archive/v${version}.tar.gz"
+    platform=$(uname -s | tr '[:upper:]' '[:lower:]')
+    arch=$(uname -m)
+    case $platform in
+    "darwin")
+        case $arch in
+        "aarch64" | "arm64")
+            url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-aarch64-apple-darwin.tar.xz"
+            ;;
+        "x86_64")
+            url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-x86_64-apple-darwin.tar.xz"
+            ;;
+        esac
+        ;;
+    "linux")
+        case $arch in
+        "armv7" | "armv7l")
+            url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-armv7-unknown-linux-musleabi.tar.xz"
+            ;;
+        "aarch64" | "arm64")
+            url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-aarch64-unknown-linux-musl.tar.xz"
+            ;;
+        "riscv64")
+            url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-riscv64gc-unknown-linux-gnu.tar.xz"
+            ;;
+        "x86_64")
+            url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-x86_64-unknown-linux-musl.tar.xz"
+            ;;
+        esac
+        ;;
+    *mingw* | *msys* | *cygwin* | *windows*)
+        case $arch in
+        "aarch64" | "arm64")
+            url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-aarch64-pc-windows-msvc.zip"
+            ;;
+        "x86_64")
+            url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-x86_64-pc-windows-msvc.zip"
+            ;;
+        esac
+        ;;
+    esac
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+    echo "* Downloading $TOOL_NAME release $version..."
+    echo "  -> url: $url"
+    echo "  -> target file: $filename"
+    curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
+    local install_type="$1"
+    local version="$2"
+    local install_path="${3%/bin}/bin"
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
+    if [ "$install_type" != "version" ]; then
+        fail "asdf-$TOOL_NAME supports release installs only"
+    fi
 
-	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    (
+        mkdir -p "$install_path"
+        cp -r "$ASDF_DOWNLOAD_PATH/"* "$install_path/"
 
-		# TODO: Assert tytanic executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+        local tool_cmd
+        tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+        test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
+        echo "$TOOL_NAME $version installation was successful!"
+    ) || (
+        # rm -rf "$install_path"
+        fail "An error occurred while installing $TOOL_NAME $version."
+    )
 }
